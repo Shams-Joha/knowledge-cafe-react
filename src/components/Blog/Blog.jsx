5e3230331495a8521b503f8a0a15de048a238975
Shams-Joha <shamsjoha3@gmail.com>
2024-10-23 01:50:09 +0600

    Add bookmark button and lift up the state

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,15 +1,17 @@
 import propTypes from 'prop-types';
 import profile1 from '../../assets/images/boy1.png'
-const Blog = ({ blog }) => {
+import { FaBookBookmark } from "react-icons/fa6";
+
+const Blog = ({ blog, handleAddToBookmark }) => {
     const { title, cover, reading_time, author,
         hashtags, posted_date } = blog;
     return (
-        <div >
-            <img src={cover}></img>
+        <div className='mb-20'>
+            <img className='w-full mb-8' src={cover}></img>
             <div className='flex justify-between items-center'>
-                <div className='flex gap-5'>
+                <div className='flex gap-5 mb-4'>
                     <img className='w-14' src={profile1} alt="" />
-                    <div>
+                    <div >
                         <h3 className='text-2xl'>{author}</h3>
                         <p>{posted_date}</p>
                     </div>
@@ -17,9 +19,10 @@ const Blog = ({ blog }) => {
                 </div>
                 <div>
                     <span>{reading_time} min read</span>
+                    <button onClick={handleAddToBookmark} className='ml-2 text-red-600 text-2xl'><FaBookBookmark /></button>
                 </div>
             </div>
-            <h2 className='text-4xl'>{title}</h2>
+            <h2 className='text-4xl mb-4'>{title}</h2>
             <p>
                 {
                     hashtags.map((hash, idx) => <span key={idx}><a href=''>#{hash}</a></span>)
--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import Blog from "../Blog/Blog";
 
 
-const Blogs = () => {
+const Blogs = ({ handleAddToBookmark }) => {
 
     const [blogs, setBlogs] = useState([]);
 
@@ -19,7 +19,9 @@ const Blogs = () => {
             <h1 className="text-4xl">Blogs {blogs.length}</h1>
             {
                 blogs.map(blog => <Blog key={blog.id}
-                    blog={blog}></Blog>)
+                    blog={blog}
+                    handleAddToBookmark={handleAddToBookmark}
+                ></Blog>)
             }
 
         </div>
