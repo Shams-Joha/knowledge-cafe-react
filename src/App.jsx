0442e911ce28138f2e5828dcd8049f5eb7d6eed5
Shams-Joha <shamsjoha3@gmail.com>
2024-10-22 23:56:36 +0600

    Created fake data

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import { useState } from 'react'
-
+import Header from './components/Header/Header'
 import './App.css'
+import Blogs from './components/Blogs/Blogs'
 
 function App() {
-  
+
 
   return (
     <>
 
-      
+      <Header></Header>
+      <Blogs></Blogs>
 
     </>
   )
 }
+
+
+
 
 export default App
